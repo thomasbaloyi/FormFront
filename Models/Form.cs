96882c856d3adeee8a0df2d8804e597379864fc6
Thomas Baloyi <baloyi.thomas@outlook.com>
2021-09-18 00:55:44 +0200

    update

---
+++ Models/Form.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormFront.Models
+{
+    public class Form
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Country { get; set; }
+        public string FavouriteColour { get; set; }
+        public  DateTime Birthday { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Comments { get; set; }
+    }
+}
