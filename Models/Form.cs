10d45fed67852e516f755dfd1e0c485b7aa2314b
Thomas Baloyi <baloyi.thomas@outlook.com>
2021-09-20 02:57:17 +0200

    add validations

--- Models/Form.cs
+++ Models/Form.cs
@@ -1,23 +1,36 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace FormFront.Models
 {
     public class Form
     {
-        public Form()
-        {
-
-        }
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Surname { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
         public string Password { get; set; }
+
+        [Required]
         public string ConfirmPassword { get; set; }
         public string Country { get; set; }
         public string FavouriteColour { get; set; }
+
+        [DataType(DataType.Date)]
         public  DateTime Birthday { get; set; }
+
+     //   [Phone]
         public int PhoneNumber { get; set; }
+
         public string Comments { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -22,12 +22,11 @@ namespace FormFront
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get;}
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
